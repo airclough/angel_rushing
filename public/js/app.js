14da492d60ee0bb077c9125235450023fb835dfb
Robby <robby.fairclough@gmail.com>
2014-05-18 02:39:36 -0700

    added / wired up requirejs

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+define(
+  [ 'backbone' ],
+  function( Backbone ) {
+    'use strict';
+
+    function init() {
+      console.log( Backbone );
+    }
+
+    return {
+      init: init
+    };
+  }
+);
